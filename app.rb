c7efe1ed0b8a634c99eb2ac8ba93598b2c7acd82
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-18 22:32:12 +0900

    fix

--- app.rb
+++ app.rb
@@ -9,12 +9,14 @@ get '/' do
   @title = '沖縄県映画上映時間一覧'
   @titles = ''
   @times  = ''
+  index = 0
 
   items = RSS::Parser.parse('public/feed.xml').items
 
   items.each do |movie|
-    @titles << "<li><a href='##{movie.title}'>#{movie.title}</a></li>\n"
-    @times << "<ul id='#{movie.title}' title='#{movie.title}'><li>#{movie.description}</li></ul>\n"
+    @titles << "<li><a href='##{index}'>#{movie.title}</a></li>\n"
+    @times << "<ul id='#{index}' title='#{movie.title}'><li>#{movie.description}</li></ul>\n"
+    index += 1
   end
 
 
