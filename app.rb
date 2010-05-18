bc06d4a5568516503aaa71ab97c2506b17ef57e5
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-18 18:18:05 +0900

    沖縄映画の RSS フィード配信に対応

--- app.rb
+++ app.rb
@@ -1,11 +1,27 @@
+require 'rss'
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+
+require 'okinawa_movies'
 
 set :haml, :format => :html5
 
 get '/' do
   @title = '沖縄県映画上映時間一覧'
-  @body = open('movies.txt').read
+  @body  = '<ul>'
+
+  rss = OkinawaMovies.rss
+  rss.items.each do |movie|
+    @body << "<li><a href='#{movie.link}'>#{movie.title}</a></li>"
+    movie.description.each { |theater| @body << "<li>#{theater}</li>" }
+  end
+
+  @body << '</ul>'
+
   haml :index
+end
+
+get '/feed.xml' do
+  OkinawaMovies.rss.to_s
 end
---
+++ okinawa_movies.rb
@@ -0,0 +1,79 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'rss'
+require 'rubygems'
+require 'nokogiri'
+
+$KCODE = 'u'
+
+class OkinawaMovies 
+  def initialize
+    @theaters_uri = [
+      'http://www.startheaters.jp/schedule',
+      'http://www.google.co.jp/movies?tid=3d1a4be489681836'
+    ]
+  end
+
+  def self.rss
+    new.rss
+  end
+
+  def rss 
+    movies = get_screen_time
+
+    RSS::Maker.make('2.0') do |maker|
+      maker.channel.about = 'http://okinawa-movie.heroku.com/feed.xml'
+      maker.channel.title = '沖縄県映画上映時間一覧'
+      maker.channel.description = '沖縄県内の映画の上映時間を配信しています'
+      maker.channel.link = 'http://okinawa-movie.heroku.com/'
+
+      maker.items.do_sort = true
+
+      movies.each do |movie|
+        maker.items.new_item do |item|
+          item.link = movie[:link]
+          item.title = movie[:title]
+          item.description = movie[:time]
+          item.date = Time.now
+        end
+      end
+    end
+  end
+
+  def get_screen_time
+    results = []
+
+    @theaters_uri.each do |theater|
+      html = Nokogiri::HTML(open(theater).read)
+
+      if theater.include? 'startheaters'
+        (html/'div.unit_block').each do |movie_info|
+          item = {}
+          item[:title] = movie_info.at('h3/a').text
+          item[:link]  = movie_info.at('div.pic_block/a[@target="_blank"]').attributes['href']
+          item[:time] = []
+
+          (movie_info/'table.set_d').each do |screen|
+            movie = ["[#{screen.at('th.cinema/img').attributes['alt']}]"]
+            (screen/'td').each do |time|
+              movie << time.text unless time.text.to_i == 0
+            end
+            item[:time] << movie.join('  ')
+          end
+          results << item
+        end
+      else
+        (html/'div.movie').each do |info|
+          item = {}
+          item[:title] = (info/'div.name/a/span[@dir="ltr"]').text
+          item[:link]  = 'http://www.sakura-zaka.com/'
+          item[:time]  = "[桜坂劇場] #{info.inner_html.scan(/(..:..+?)</).last}"
+          results << item
+        end
+      end
+    end
+
+    results
+  end
+end
