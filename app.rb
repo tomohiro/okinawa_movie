45d99feaab94767bfade2f39e2bf0a411ffe6a56
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-18 09:55:48 +0900

    fix

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
+require 'rubygems'
 require 'sinatra'
 require 'haml'
 
 set :haml, :format => :html5
 
 get '/' do
-  @title = 'Fuck, World!'
+  @title = '沖縄県映画上映時間一覧'
   @body = open('movies.txt').read
   haml :index
 end
