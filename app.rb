c0ccb2154fd0d3d0b0e539d6d343beebe0aa7629
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-19 11:27:04 +0900

    add Google Analytics, Google Webmaster Tools

--- app.rb
+++ app.rb
@@ -6,19 +6,36 @@ require 'haml'
 set :haml, :format => :html5
 
 get '/' do
-  @title = '沖縄県映画上映時間一覧'
-  @titles = ''
+  @movies = ''
   @times  = ''
+  @google = google
   index = 0
 
   items = RSS::Parser.parse('public/feed.xml').items
 
   items.each do |movie|
-    @titles << "<li><a href='##{index}'>#{movie.title}</a></li>\n"
+    @movies << "<li><a href='##{index}'>#{movie.title}</a></li>\n"
     @times << "<ul id='#{index}' title='#{movie.title}'><li>#{movie.description}</li></ul>\n"
     index += 1
   end
 
-
   haml :index
+end
+
+def google
+<<-GOOGLE
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-13275958-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+GOOGLE
 end
