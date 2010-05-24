9cae4df0493ad7f46e4a5b48cf4e1de628a93066
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-24 19:50:17 +0900

    UI を修正．Google Analytics を静的ファイルにした

--- app.rb
+++ app.rb
@@ -3,22 +3,26 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
-
 get '/' do
   @movies = ''
   @times  = ''
-  @google = google
 
   index = 0
   movies do |movie|
     if movie.description.nil?
-      @movies << "<li class='group'>#{movie.title}</li>"
+      @movies << "<li class='group'>#{movie.title}</li>\n"
     else
       @movies << "<li><a href='##{index}'>#{movie.title}</a></li>\n"
-      @times  << "<ul id='#{index}' title='#{movie.title}'><li>#{movie.description}</li></ul>\n"
+      @times  << "<ul id='#{index}' title='映画館別上映時間'>\n"
+      @times  << "<li class='group'><a href='#{movie.link}' target='_blank' title='#{movie.title}'>#{movie.title}</a></li>\n"
+      movie.description.split('<br>').each do |time|
+        @times << "<li>#{time}</li>\n"
+      end
+      @times << "</ul>\n"
     end
     index += 1
   end
+
   set :haml, {:format => :html5}
   haml :index
 end
@@ -37,22 +41,4 @@ end
 
 def movies
   RSS::Parser.parse('public/feed.xml').items.each { |m| yield m }
-end
-
-def google
-<<-GOOGLE
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-13275958-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-GOOGLE
 end
