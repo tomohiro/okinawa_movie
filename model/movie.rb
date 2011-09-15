f60e1c6625c6d9ea5d39055a1a05d079b72f7cbf
Tomohiro, TAIRA <tomohiro.t@gmail.com>
2011-09-15 18:48:03 +0900

    not use select method

--- model/movie.rb
+++ model/movie.rb
@@ -29,12 +29,12 @@ class Movie < Sequel::Model
 
     def startheaters
       # url: http://www.startheaters.jp/schedule
-      select(:id, :title).filter(:url.like('%star%')).group(:title) #.all
+      filter(:url.like('%star%')).group(:title)
     end
 
     def sakurazaka
       # url: http://www.google.com/movies?tid=3d1a4be489681836&near=%E9%82%A3%E8%A6%87%E5%B8%82
-      select(:id, :title).filter(:url.like('%google%')).group(:title) #.all
+      filter(:url.like('%google%')).group(:title)
     end
 
     def showtime(id)
