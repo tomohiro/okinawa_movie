41244861e5e5759de59d9b6cf33b5b2eb3ea881a
Tomohiro TAIRA <tomohiro.t@gmail.com>
2013-12-25 22:05:45 +0900

    Update scraping process for new startheaters' site

--- cron/okinawa_movies.rb
+++ cron/okinawa_movies.rb
@@ -2,8 +2,6 @@
 # encoding:utf-8
 
 require 'open-uri'
-require 'rss'
-require 'rubygems'
 require 'nokogiri'
 
 require 'model/movie.rb'
@@ -11,71 +9,73 @@ require 'model/movie.rb'
 class OkinawaMovies
   def initialize
     @theaters_uri = [
-      'http://www.startheaters.jp/schedule',
-      "http://www.google.com/movies?tid=3d1a4be489681836&near=#{URI.escape('那覇市')}"
+      'http://www.startheaters.jp/now_showing.html',
+       "http://www.google.com/movies?tid=3d1a4be489681836&near=#{URI.escape('那覇市')}"
     ]
   end
 
-  def migrate(reset = true)
+  def update(reset = true)
     Movie.reset if reset
 
     get_showtime.each do |showtime|
-      showtime[:time].split('<br>').each do |info|
-        info = info.split(' ')
-        theater = info.shift
-
-        info.each do |time|
-          Movie.create({
-            :title   => showtime[:title].toutf8,
-            :theater => theater.toutf8,
-            :poster  => '',
-            :url     => showtime[:source_url].toutf8,
-            :start   => (time.split([0XFF5E].pack('U'))[0]).toutf8,
-            :end     => (time.split([0XFF5E].pack('U'))[1]) || '',
-            :date    => Time.now
-          })
-        end
-      end
+      Movie.create(showtime)
     end
   end
 
   def get_showtime
-    results = []
+    showtimes = []
 
     @theaters_uri.each do |theater|
-      html = Nokogiri::HTML(open(theater).read)
+      html = Nokogiri::HTML(open(theater))
 
       if theater.include? 'startheaters'
-        (html/'div.unit_block').each do |movie_info|
-          item = {}
-          item[:source_url] = theater
-          item[:source_content] = 'スターシアターズ'
-          item[:title] = movie_info.at('h3/a').text.gsub('　', '')
-          item[:link]  = movie_info.at('div.pic_block/a[@target="_blank"]').attributes['href'] rescue theater
-          item[:time]  = ''
+        html.search('.movieThumbBox').each do |movie|
+          title  = movie.at('.movieTtl').text
+          url    = "http://www.startheaters.jp/#{movie.at('.detailBtn/a').attributes['href']}"
+          poster = movie.at('.flyer/img').attributes['src'].value
 
-          (movie_info/'table.set_d').each do |screen|
-            movie = ["[#{screen.at('th.cinema/img').attributes['alt']}]"]
-            (screen/'td').each do |time|
-              movie << time.text unless time.text.to_i == 0
+          movie_detail_page = Nokogiri::HTML(open(url))
+          movie_detail_page.search('.MovieScheduleBox').each do |schedule|
+            theater = theater_name(schedule.at('.theaterInfo/dd').attributes['class'].value)
+            schedule.search('.sowtimesList/li').each do |showtime|
+              showtimes << {
+                title: title,
+                url: url,
+                poster: poster,
+                theater: theater,
+                start: showtime.at('.startTime').text,
+                end: showtime.at('.endTime').text.gsub([0XFF5E].pack('U'), ''),
+              }
             end
-            item[:time] << movie.join('  ') + '<br>'
           end
-          results << item
         end
       else
-        (html/'div.movie').each do |info|
-          item = {}
-          item[:source_url] = theater
-          item[:source_content] = '桜坂劇場'
-          item[:title] = (info/'div.name/a').text
-          item[:link]  = 'http://www.sakura-zaka.com/'
-          item[:time]  = "[桜坂劇場] #{info.inner_html.scan(/(..:..+?)</).last}"
-          results << item
+        html.search('.movie').each do |movie|
+          movie.search('.times/span').each do |showtime|
+            showtimes << {
+              title: movie.at('.name/a').text,
+              url: "http://www.google.com#{movie.at('.name/a').attributes['href'].value}",
+              poster: '',
+              theater: '桜坂劇場',
+              start: "#{showtime.text.gsub('&nbsp', '').gsub(' ', '')}",
+              end: '',
+            }
+          end
         end
       end
     end
 
-    results
+    showtimes
   end
+
+  private
+    def theater_name(theater_id)
+      theaters = {
+        'theaterQ' => 'シネマQ',
+        'theaterS' => 'ミハマ7プレックス',
+        'theater7' => 'サザンプレックス',
+        'theaterP' => 'シネマパレット'
+      }
+      theaters[theater_id]
+    end
 end
